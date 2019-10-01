9098673e79730bc6458a5fe6151c51014e1453a4
Piotr Komosa <komosa.piotr@gmail.com>
2019-10-01 22:20:20 +0200

    Chapter 1.2 - Constructor added for Car Class, Main Cars updated.

--- src/Car.java
+++ src/Car.java
@@ -1,9 +1,9 @@
-public class Car
-{
-
+public class Car {
     String company;
     String model;
     int age;
-
-
+    Car(String cmp, String md, int a) {
+    company = cmp;
+    model = md;
+    age = a; }
 }
