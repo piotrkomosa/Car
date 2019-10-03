21e1d8616989396dad5031576b79eef1852de962
Piotr Komosa <komosa.piotr@gmail.com>
2019-10-03 12:19:40 +0200

    Chapter 1.4 - Code edited.

--- src/Car.java
+++ src/Car.java
@@ -2,7 +2,10 @@ public class Car {
     String company;
     String model;
     int age;
+
     Car(String cmp, String md, int a) {
-    company = cmp;
-    model = md;
-    age = a; }}
+        company = cmp;
+        model = md;
+        age = a;
+    }
+}
--- src/Cars.java
+++ src/Cars.java
@@ -1,11 +1,15 @@
 public class Cars { public static void main(String[] args) {
         Car car1 = new Car("VW","Golf",2006);
-        car1.company = "VW";
-        car1.model = "Golf";
-        car1.age = 2006;
+                car1.company = "VW";
+                car1.model = "Golf";
+                car1.age = 2006;
+
         Car car2 = new Car("Ford", "Focus", 2015);
-        car2.company = "Ford";
-        car2.model = "Focus";
-        car2.age = 2015;
+                car2.company = "Ford";
+                car2.model = "Focus";
+                car2.age = 2015;
+
         System.out.println("Samochod pierwszy: " + car1.company + " " + car1.model + ", rocznik " + car1.age);
-        System.out.println("Samochod pierwszy: " + car2.company + " " + car2.model + ", rocznik " + car2.age); }}
+        System.out.println("Samochod pierwszy: " + car2.company + " " + car2.model + ", rocznik " + car2.age);
+        }
+}
